88632a437db9fcf79033f89cdcb36423613d5c6b
yash sharma <yash@saal.ai>
2025-06-30 14:52:03 +0400

    Refactor memory handling and improve stream graph updates in chatbot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 
-
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, START, END
 from langchain.chat_models import init_chat_model
 from langchain_tavily import TavilySearch
@@ -10,6 +10,10 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 load_dotenv()
 
+# Memory Saver
+# This is used to save the state of the conversation, so that it can be resumed later
+# You can use any other memory saver, like Redis, or a database
+memory = MemorySaver()
 
 # Tool
 tool = TavilySearch(max_results=2)
@@ -45,13 +49,18 @@ graph_builder.add_conditional_edges(
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
 
-
+config = {"configurable": {"thread_id": "1"}}
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+    # The config is the **second positional argument** to stream() or invoke()!
+    events = graph.stream(
+        {"messages": [{"role": "user", "content": user_input}]},
+        config,
+        stream_mode="values",
+    )
+    for event in events:
+        event["messages"][-1].pretty_print()
 
 while True:
     try:
